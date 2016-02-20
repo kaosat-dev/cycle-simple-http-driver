b3a4c5c0b3ef09d7b7940636fb149c31c9c88b53
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-02-20 14:54:55 +0100

    chore(whitespacing): modified whitespace to 2 instead of 4, as should have been

--- src/index.js
+++ src/index.js
@@ -6,85 +6,85 @@ if(isNode){
 }
 
 export function createResponse$(options){
-    const defaults = {
-        method:'get'
-        ,encoding:'utf8'
-        ,mimeType:'text/plain; charset=x-user-defined'
-        ,responseType:undefined
-        ,timeout:undefined
-        ,send:undefined
-    }
-    options = Object.assign({}, defaults, options)
+  const defaults = {
+    method:'get'
+    ,encoding:'utf8'
+    ,mimeType:'text/plain; charset=x-user-defined'
+    ,responseType:undefined
+    ,timeout:undefined
+    ,send:undefined
+  }
+  options = Object.assign({}, defaults, options)
 
-    let obs = new Rx.Subject()
+  let obs = new Rx.Subject()
 
-    let request = new XMLHttpRequest()
+  let request = new XMLHttpRequest()
 
-    function handleProgress(e){
-        [e]
-            .filter(e=>e.lengthComputable)
-            .forEach(function(e){
-                obs.onNext({progress: (e.loaded / e.total),total:e.total})
-            })
-    }
-    function handleComplete(e){
-        let response = request.response || request.responseText
+  function handleProgress(e){
+    [e]
+      .filter(e=>e.lengthComputable)
+      .forEach(function(e){
+          obs.onNext({progress: (e.loaded / e.total),total:e.total})
+      })
+  }
+  function handleComplete(e){
+    let response = request.response || request.responseText
 
-        response = options.responseType === 'json' ? JSON.parse(response) : response
-        obs.onNext({response})
-        obs.onCompleted()
-    }
+    response = options.responseType === 'json' ? JSON.parse(response) : response
+    obs.onNext({response})
+    obs.onCompleted()
+  }
 
-    function handleError(e){
-      console.log("error in httpDriver",e)
-      obs.onError(e)
-    }
+  function handleError(e){
+    console.log("error in httpDriver",e)
+    obs.onError(e)
+  }
 
-    function handleAbort(e){
-      console.log("abort in httpDriver",e)
-      obs.onError(e)
-    }
+  function handleAbort(e){
+    console.log("abort in httpDriver",e)
+    obs.onError(e)
+  }
 
-    function handleTimeout(e){
-      console.log("timeout in httpDriver",e)
-      obs.onError(e)
-    }
+  function handleTimeout(e){
+    console.log("timeout in httpDriver",e)
+    obs.onError(e)
+  }
 
-    request.addEventListener("progress", handleProgress)
-    request.addEventListener("load"    , handleComplete)
-    request.addEventListener("error"   , handleError)
-    request.addEventListener("abort"   , handleError)
-    request.addEventListener("timeout" , handleTimeout)
+  request.addEventListener("progress", handleProgress)
+  request.addEventListener("load"    , handleComplete)
+  request.addEventListener("error"   , handleError)
+  request.addEventListener("abort"   , handleError)
+  request.addEventListener("timeout" , handleTimeout)
 
 
-    request.open(options.method,options.url, true)
-    if ((options.mimeType !== null) && (request.overrideMimeType !== null)) {
-        request.overrideMimeType(options.mimeType)
-    }
-    request.timeout      = options.timeout
-    request.responseType = options.responseType
+  request.open(options.method,options.url, true)
+  if ((options.mimeType !== null) && (request.overrideMimeType !== null)) {
+    request.overrideMimeType(options.mimeType)
+  }
+  request.timeout      = options.timeout
+  request.responseType = options.responseType
 
-    request.send(options.send)
+  request.send(options.send)
 
-    return obs
+  return obs
 }
 
 
 export default function makeHttpDriver({eager = false} = {eager: false}){
 
-    return function httpDriver(request$){
-        let response$$ = request$
-            .map(reqOptions => {
-                let response$ = createResponse$(reqOptions)
-                if (eager || reqOptions.eager) {
-                    response$ = response$.replay(null, 1)
-                    response$.connect()
-                }
-                response$.request = reqOptions
-                return response$
-            })
-            .replay(null, 1)
-        response$$.connect()
-        return response$$
-    }
+  return function httpDriver(request$){
+    let response$$ = request$
+      .map(reqOptions => {
+        let response$ = createResponse$(reqOptions)
+        if (eager || reqOptions.eager) {
+            response$ = response$.replay(null, 1)
+            response$.connect()
+        }
+        response$.request = reqOptions
+        return response$
+      })
+      .replay(null, 1)
+    response$$.connect()
+    return response$$
+  }
 }
